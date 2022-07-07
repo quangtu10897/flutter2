be8cf82ad14147654ea075dabe90ed911bab7cb6
tomsoia <tomsoia@tomsoias-MacBook-Pro.local>
2022-07-07 15:19:50 +0700

    futtler app

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,110 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-        home: Scaffold(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("just a name"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.red,
       ),
-      body: Center(
-        child: Text("tutu"),
-        child: Text(
-          "Hello tu",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.blue,
+      body: Row(
+        children: [
+          Expanded(
+              flex: 3, child: Image(image: AssetImage("assets/milky1.jpeg"))),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.pinkAccent,
+              child: Text("one"),
+            ),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.blueAccent,
+              child: Text("two"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.greenAccent,
+              child: Text("three"),
+            ),
+          ),
+        ],
       ),
+
+      /*Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Text("man"),
+              Text("of"),
+              Text("steel"),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.deepOrange,
+            child: Text("one"),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            color: Colors.lightGreen,
+            child: Text("two"),
+          ),
+          Container(
+            padding: EdgeInsets.all(30),
+            color: Colors.pink,
+            child: Text("three"),
+          )
+        ],
+      ),*/
+
+      //SingleChildScrollView(
+      //scrollDirection: Axis.horizontal,
+      /*  Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("hello world"),
+          FlatButton(
+            onPressed: () {},
+            child: Text("Click me"),
+            color: Colors.cyan,
+          ),
+          Container(
+            child: Text("container"),
+            padding: EdgeInsets.all(10),
+            color: Colors.blueAccent,
+          ),
+          /* Container(
+              child: Image(image: AssetImage("assets/milky1.jpeg")),
+              height: 300,
+              width: 300,
+            ),*/
+        ],
+      ),*/
+      //),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          print("clicked");
+        },
         child: Text("click"),
         backgroundColor: Colors.red,
       ),
-    )));
+    );
+  }
+}
